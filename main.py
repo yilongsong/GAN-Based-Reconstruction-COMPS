faf566df8933fb48d67828a9703a45078230fede
Daisuke Yamada <daisukeyamada@Daisukes-MacBook-Pro.local>
2022-10-20 20:24:42 -0500

    added result

--- main.py
+++ main.py
@@ -30,9 +30,9 @@ def main():
     
     count = 0
     for img in images:
-        if count >= 70:
+        if count >= 101:
             break
-        elif count < 60:
+        elif count < 90:
             count += 1
             continue
         print('Start reconstruction on ' + img)
