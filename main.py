55286d6a113c1102abe0a4113048053f69a0e071
Daisuke Yamada <daisukeyamada@Daisukes-MacBook-Pro.local>
2022-10-25 09:54:54 -0500

    added fft

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ from model import run_model
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--GAN', type=str, required = True, choices=['PGGAN', 'DCGAN'], help='type of pre-trained GAN: PGGAN or DCGAN')
-    parser.add_argument('--rate', type=float, required=True, choices=[4,8,16,32,0.1,0.3,0.5], help='rate(scale/ratio) of the task. 4, 8, 16, or 32 for scale, and 0.1, 0.3, or 0.5 for compression ratio')
+    parser.add_argument('--scale', type=float, required=False, choices=[4,8,16,32], help='scale of the task: 4, 8, 16, or 32 for scale')
+    parser.add_argument('--ratio', type=float, required=False, choices=[0.1,0.3,0.5,0.7], help='ratio of the task: 0.1, 0.3, 0.5, or 0.7')
     parser.add_argument('--noise', type=int, required=True, choices=[0,10,40], help='noise level of y: 0, 10, or 40')
     parser.add_argument('--task', type=str, required=True, choices=['FFT', 'Bicubic'], help='task to be performed: Bicubic or FFT')
     parser.add_argument('--save_images', action='store_true', help='include this argument to save the result images')
