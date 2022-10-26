a45cffbbfa889df42293324173a5349497988f89
Daisuke Yamada <daisukeyamada@Daisukes-MacBook-Pro.local>
2022-10-26 14:29:11 -0500

    added subfolders

--- main.py
+++ main.py
@@ -18,7 +18,9 @@ def parse_args():
     parser.add_argument('--ratio', type=float, required=False, choices=[0.1,0.3,0.5,0.7], help='ratio of FFT: 0.1, 0.3, 0.5, or 0.7')
     parser.add_argument('--noise', type=int, required=True, choices=[0,10,40], help='noise level of y: 0, 10, or 40')
     parser.add_argument('--task', type=str, required=True, choices=['FFT', 'Bicubic', 'Blur'], help='task to be performed: FFT, Bicubic, or Blur')
-    parser.add_argument('--skip_csgm', action='store_true', help='include this argument to skip csgm step')
+    parser.add_argument('--csgm_itr', type=int, required=True, choices=[0,500,1000,1500,1800], help='number of iterations for CSGM')
+    parser.add_argument('--ia_itr', type=int, required=True, choices=[300,500,600,800,1100,1300,2100,2300], help='number of iterations for IA')
+    parser.add_argument('--test_folder', type=str, required=True, choices=['Whole', 'Object', 'DarkHair', 'LightHair', 'NoHair', 'DarkSkin', 'LightSkin'], help='folder of images that you want to run this model on')
     parser.add_argument('--save_images', action='store_true', help='include this argument to save the result images')
     args = parser.parse_args()
 
@@ -27,7 +29,9 @@ def parse_args():
     if args.task != 'Blur':
         folder += '_' + (str(args.scale) + 'S' if args.scale != None else str(args.ratio) + 'R')
     if args.skip_csgm:
-        folder += '_Skip'
+        folder += ('_CSGM' + str(args.csgm_itr)
+    if args.test_folder != 'Whole':
+        folder += ('_' + args.test_folder)
     folder += '/'
 
     # create a dict of parameters
@@ -36,8 +40,11 @@ def parse_args():
         'rate': 1/args.scale if args.scale != None else args.ratio,
         'noise_level': args.noise/255,
         'task': args.task,
+        'CSGM_itr': args.csgm_itr,
+        'IA_itr': args.is_itr,
         'parent_path': folder,
-        'skip_csgm': args.skip_csgm,
+        'test_folder': args.test_folder,
+        'skip_csgm': True if args.csgm_itr == 0 else False,
         'save_images': args.save_images
     }
 
@@ -45,13 +52,27 @@ def parse_args():
     if args.GAN == 'PGGAN':
         params['img_dir'] = './Images/CelebA_HQ'
         params['weights_path'] = 'PGGAN_weights.pth'
-        params['CSGM_itr'] = 1800
         params['CSGM_lr'] = 0.1
-        params['IA_itr'] =  1800 + (500 if args.task == 'FFT' else 300) if args.skip_csgm else (500 if args.task == 'FFT' else 300)
         params['IA_z_lr'] = 0.0001
         params['IA_G_lr'] = 0.001
 
     return params
+
+def get_test_folder(folder):
+    if folder == 'Whole':
+        return [str(i) for i in range(0, 100)]
+    elif folder == 'Object':
+        return ['7', '11', '45', '47', '48', '61', '66', '69', '73', '74']
+    elif folder == 'DarkHair':
+        return ['4', '5', '6', '10', '15', '16', '18', '50', '51', '55']
+    elif folder == 'LightHair':
+        return ['0', '1', '3', '7', '12', '13', '46', '62', '76', '92']
+    elif folder == 'NoHair':
+        return ['15', '30', '45', '47', '59', '89']
+    elif folder == 'DarkSkin':
+        return ['7', '31', '38', '39', '42', '50', '90', '100', '101', '102']
+    elif folder == 'LightSkin':
+        return [str(i) for i in range(0, 10)]
 
 def reset_weights(weights_path):
     src_path = './saved_weights/' + weights_path
@@ -74,11 +95,19 @@ def main():
     images = [f for f in files if ('.jpg' or '.png') in f]
     images.sort(key=natural_keys)
 
+    # get test_folder
+    test_folder = get_test_folder(params['test_folder'])
+
     # Run the model on each image
+    num_img_saved = 0
     for img in images:
         print('Start reconstruction on ' + img)
-        run_model(img, params)
-        reset_weights(params['weights_path'])
+        if num_img_saved >= 5:
+            params['save_images'] = False
+        if img[:-4] in test_folder:
+            run_model(img, params)
+            reset_weights(params['weights_path'])
+            num_img_saved += 1
 
 if __name__ == '__main__':
     main()
--- test.py
+++ test.py
@@ -1,22 +1,31 @@
-import torch
-import torchvision
-from torchvision import transforms
-from itertools import product
-from PIL import Image
-import warnings
-import numpy as np
-warnings.filterwarnings("ignore")
+# import torch
+# import torchvision
+# from torchvision import transforms
+# from itertools import product
+# from PIL import Image
+# import warnings
+# import numpy as np
+# warnings.filterwarnings("ignore")
 
-convert_to_tensor = transforms.ToTensor()
-convert_to_PIL = transforms.ToPILImage()
+# convert_to_tensor = transforms.ToTensor()
+# convert_to_PIL = transforms.ToPILImage()
 
-# Original
-I_PIL = Image.open('./Images/CelebA_HQ/0.jpg')
-I = convert_to_tensor(I_PIL)
-x = torch.unsqueeze(I, 0)
+# # Original
+# I_PIL = Image.open('./Images/CelebA_HQ/0.jpg')
+# I = convert_to_tensor(I_PIL)
+# x = torch.unsqueeze(I, 0)
 
-from A import A
-y = A.blur_A(x)
-y = torch.clamp(y, 0, 1)
-y = convert_to_PIL(y[0])
-y.show()
+# from A import A
+# y = A.blur_A(x)
+# y = torch.clamp(y, 0, 1)
+# y = convert_to_PIL(y[0])
+# y.show()
+x = 1
+if x == 2:
+    y = 1
+def sum(y):
+    if y == None:
+        print('hi')
+sum(y)
+
+
