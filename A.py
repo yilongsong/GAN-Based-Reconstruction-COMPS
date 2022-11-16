583ffebfdec0dd7e06c81dcabcdd46c8e1f690dc
Daisuke Yamada <daisukeyamada@Daisukes-MacBook-Pro.local>
2022-11-16 14:34:58 -0600

    project complete

--- A.py
+++ A.py
@@ -1,6 +1,5 @@
 '''
     Where we implement measurement metricies A
-
     https://discuss.pytorch.org/t/pytorch-pil-to-tensor-and-vice-versa/6312
 '''
 import torch
--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def parse_args():
     parser.add_argument('--task', type=str, required=True, choices=['FFT', 'Bicubic', 'Blur'], help='task to be performed: FFT, Bicubic, or Blur')
     parser.add_argument('--csgm_itr', type=int, required=True, choices=[0,500,1000,1500,1800], help='number of iterations for CSGM')
     parser.add_argument('--ia_itr', type=int, required=True, choices=[300,500,600,800,1100,1300,2100,2300], help='number of iterations for IA')
-    parser.add_argument('--test_folder', type=str, required=True, choices=['Half', 'Whole', 'Object', 'DarkHair', 'LightHair', 'NoHair', 'DarkSkin', 'LightSkin', 'Us'], help='folder of images that you want to run this model on')
+    parser.add_argument('--test_folder', type=str, required=True, choices=['Half', 'Whole', 'Object', 'DarkHair', 'LightHair', 'NoHair', 'DarkSkin', 'LightSkin'], help='folder of images that you want to run this model on')
     parser.add_argument('--save_images', action='store_true', help='include this argument to save the result images')
     args = parser.parse_args()
 
@@ -84,9 +84,6 @@ def get_test_folder(folder):
         return [str(i) for i in range(0, 50)]
     elif folder == 'Single':
         return ['0']
-    elif folder == 'Us':
-        ['106', '107', '108']
-
 
 def reset_weights(weights_path):
     src_path = './saved_weights/' + weights_path
